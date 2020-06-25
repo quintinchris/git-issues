23cec465971a1499951cb5034fe63d48f67349b2
QuintinChris <cwq23drexel@gmail.com>
2020-06-25 11:58:32 -0400

    got SMS working, need to define req.body dynamically

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,102 +1,54 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <input type="text" placeholder="Enter your text here" v-model='message' value='message'/>
+    <a v-on:click="sendSMS(message)">
+      <i class="btn far fa-arrow-alt-circle-up"></i>
+    </a>
   </div>
 </template>
 
 <script>
+const API_URL = 'http://localhost:8080/';
+
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
     };
+  },
+  mounted() {
+
+  },
+  methods: {
+    sendSMS(text) {
+      // eslint-disable-next-line no-console
+      console.log(text);
+      fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(text),
+      }).then(response => response.json())
+        .then((result) => {
+          if (result.details) {
+            const error = result.details
+              .map(detail => detail.message)
+              .join('.');
+            this.error = error;
+          } else {
+            this.error = '';
+          }
+        });
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -109,5 +61,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.btn:hover {
+  cursor: pointer;
 }
 </style>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
@@ -10,16 +11,26 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(helmet());
 app.use(cors());
-
-// Twilio SMS setup
-client.messages
-  .create({
-     body: '', //`${repo.issue}`
-     from: `${process.env.TWILIO_PHONE_NUM}`,
-     to: '' //`${user.number}`
-   })
-  .then(message => console.log(message.sid));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var jsonParser = bodyParser.json();
+//app.use(bodyParser.text());
+//app.use(bodyParser.raw());
+//app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT, () => console.log(`Example app listening at http://localhost:${process.env.PORT}`));
+app.post('/', function (req, res) {
+    console.log(req.body);
+    /*
+    client.messages
+        .create({
+            body: `${req.body}`, //`${repo.issue}`
+            from: process.env.TWILIO_PHONE_NUM,
+            to: 16072427828 //`${user.number}`
+        })
+        .then(message => console.log(message.sid));
+        */
+});
+
+app.listen(process.env.PORT, () => console.log(`Listening at http://localhost:${process.env.PORT}`));
